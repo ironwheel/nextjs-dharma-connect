47ac47349777cd58b876713495abb1e6279ff9c1
Robert Taylor <ironwheel.13@gmail.com>
2025-07-13 22:51:54 -0400

    Access-Control-Allow-Credentials

--- packages/api/lib/apiMiddleware.ts
+++ packages/api/lib/apiMiddleware.ts
@@ -63,6 +63,10 @@ export const apiMiddleware = nextConnect<NextApiRequest, NextApiResponse>()
           });
           console.log("SETTING COOKIE:", cookieStr);
           res.setHeader('Set-Cookie', cookieStr);
+          res.setHeader('Access-Control-Allow-Credentials', 'true');
+          res.setHeader('Access-Control-Allow-Origin', 'https://email-manager.slsupport.link');
+          // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+          // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         }
         if (checkResult.status !== 'authenticated') {
           res.status(401).json({ error: 'Unauthorized' });
